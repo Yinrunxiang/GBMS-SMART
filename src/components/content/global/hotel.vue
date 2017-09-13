7d40b92ddd3c008bfaaedeeeeb49529c3a4770f8
JensenYin <1419323396@qq.com>
2017-09-13 11:16:26 +0800

    调整楼层图片

--- src/components/content/global/hotel.vue
+++ src/components/content/global/hotel.vue
@@ -11,7 +11,7 @@
             <div class="container-home">
                 <div class="build">
                     <a class="build-img">
-                        <img src="../../../assets/images/build.jpg"><img>
+                        <img src="../../../assets/images/build.jpg">
                     </a>
                     <!-- <p class="p-title">Build</p> -->
                 </div>
@@ -21,43 +21,8 @@
                     <!-- <p class="p-title">Floor</p> -->
                 </div>
             </div>
-            <!-- <div class="container-floor" :span="24">
-                                                                                                                                                    <div>
-                                                                                                                                                        <div class="floor-row" :span="24">
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                        </div>
-                                                                                                                                                        <div class="floor-row" :span="24">
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                        </div>
-                                                                                                                                                        <div class="floor-aisle" :span="24">Aisle</div>
-                                                                                                                                                        <div class="floor-row row">
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                        </div>
-                                                                                                                                                        <div class="floor-row" :span="24">
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                            <div class="floor-room" :span="6">ROOM</div>
-                                                                                                                                                        </div>
-                                                                                                                                                    </div>
-                                                                                                                                                </div>
-                                                                                                                                                <div class="container-room" :span="24">
-                                                                                                                                                    <div class="parlor  room" :span="24">Parlor</div>
-                                                                                                                                                    <div class="livingroom room" :span="16">Living room</div>
-                                                                                                                                                    <div class="bathroom room"  :span="8">Bathroom</div>
-                                                                                                                                                </div> -->
         </div>
-
-        <div v-show="showFloor" class="mode">
+        <div v-show="showFloor" class="floor-content">
             <div class="floorImga">
                 <div class="room1" @click="roomClick('101')"></div>
                 <div class="room2" @click="roomClick('101')"></div>
@@ -313,9 +278,13 @@ export default {
 
 .build-img {
     display: inline-block;
-    width: 100px;
-    height: 300px;
+    width: 300px;
+    height: 500px;
     margin: 30px auto;
+}
+.build-img img{
+    width: 100%;
+    height: 100%;
 }
 
 .p-title {
@@ -326,7 +295,7 @@ export default {
 .floor {
     position: absolute;
     right: 50px;
-    top: 30px;
+    bottom:  75px;
     width: 200px;
     height: 300px;
     margin: 0 30px 0 100px;
@@ -337,9 +306,10 @@ export default {
     height: 30px;
     line-height: 30px;
     margin: 3px auto;
-    background-color: rgb(88, 183, 255);
+    background-color: rgba(0, 0, 0,0.7);
+    /* background-color: rgb(88, 183, 255); */
     border: 1px solid #fff;
-    border-radius: 5px;
+    /* border-radius: 5px; */
     text-align: center;
     color: #fff;
     font-size: 14px
@@ -382,14 +352,18 @@ export default {
 .bathroom {
     height: 200px
 }
-
+.floor-content{
+    width: 100%;
+    height: 740px;
+    background-color: #fff;
+}
 
 .floorImga {
     position: relative;
-    width: 1031px;
-    height: 609px;
-    margin: 0px auto;
-    background-image: url("../../../assets/images/hotal.jpg");
+    width: 1020px;
+    height: 740px;
+    /* margin: 0px auto; */
+    background-image: url("../../../assets/images/floor.jpg");
     background-repeat: no-repeat;
     background-size: 100% 100%;
     -moz-background-size: 100% 100%;
@@ -419,105 +393,97 @@ export default {
 
 .room1 {
     position: absolute;
-    left: 10px;
-    top: 10px;
-    width: 108px;
-    height: 226px;
+    left: 0;
+    top: 0;
+    width: 170px;
+    height: 335px;
 }
 
 .room2 {
     position: absolute;
-    left: 123px;
-    top: 10px;
-    width: 264px;
-    height: 226px;
+    left: 170px;
+    top: 0;
+    width: 170px;
+    height: 335px;
 }
 
 .room3 {
     position: absolute;
-    left: 393px;
-    top: 10px;
-    width: 118px;
-    height: 226px;
+    left: 340px;
+    top: 0;
+    width: 170px;
+    height: 335px;
 }
 
 .room4 {
     position: absolute;
-    left: 519px;
-    top: 10px;
-    width: 246px;
-    height: 226px;
+    left: 510px;
+    top: 0;
+    width: 170px;
+    height: 335px;
 }
 
 .room5 {
     position: absolute;
-    left: 772px;
-    top: 10px;
-    width: 144px;
-    height: 226px;
+    left: 680px;
+    top: 0;
+    width: 170px;
+    height: 335px;
 }
 
 .room6 {
     position: absolute;
-    left: 922px;
-    top: 10px;
-    width: 100px;
-    height: 226px;
+    left: 850px;
+    top: 0;
+    width: 170px;
+    height: 335px;
 }
 
 .room7 {
     position: absolute;
-    left: 10px;
-    top: 326px;
-    width: 250px;
-    height: 231px;
+    left: 0;
+    top: 400px;
+    width: 170px;
+    height: 335px;
 }
 
 .room8 {
     position: absolute;
-    left: 266px;
-    top: 326px;
-    width: 120px;
-    height: 275px;
+    left: 170px;
+    top: 400px;
+    width: 170px;
+    height: 335px;
 }
 
 .room9 {
     position: absolute;
-    left: 393px;
-    top: 326px;
-    width: 120px;
-    height: 275px;
+    left: 340px;
+    top: 400px;
+    width: 170px;
+    height: 335px;
 }
 
 .room10 {
     position: absolute;
-    left: 520px;
-    top: 326px;
-    width: 120px;
-    height: 275px;
+    left: 510px;
+    top: 400px;
+    width: 170px;
+    height: 335px;
 }
 
 .room11 {
     position: absolute;
-    left: 645px;
-    top: 326px;
-    width: 120px;
-    height: 275px;
+    left: 680px;
+    top: 400px;
+    width: 170px;
+    height: 335px;
 }
 
 .room12 {
     position: absolute;
-    left: 770px;
-    top: 326px;
-    width: 120px;
-    height: 275px;
-}
-
-.room13 {
-    position: absolute;
-    left: 897px;
-    top: 326px;
-    width: 120px;
-    height: 275px;
+    left: 850px;
+    top: 400px;
+    width: 170px;
+    height: 335px;
 }
 </style>
